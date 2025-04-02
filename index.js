16a81a026fba25b20ca2490dd2215b07a7097cd0
komed3 <pk97@outlook.de>
2025-04-03 00:03:51 +0200

    add methods for setting bounds, maximum ticks, and logarithmic base

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * lightweight npm package to create logarithmic scales
  * 
  * @author komed3 (Paul Köhler)
- * @version 1.0.1
+ * @version 1.0.0
  * @license MIT
  */
 
@@ -12,10 +12,71 @@
 module.exports = class LogScale {
 
     is = false;
+    base = 10;
 
     constructor ( _low, _high, _ticks, _base ) {
 
-        //
+        if ( _low && _high ) {
+
+            this.setBounds( _low, _high );
+
+        }
+
+        if ( _ticks ) {
+
+            this.setMaxTicks( _ticks );
+
+        }
+
+        if ( _base ) {
+
+            this.setBase( _base );
+
+        }
+
+    };
+
+    /**
+     * set lower / upper bounds
+     * 
+     * @param {Number} low lower bound
+     * @param {Number} high upper bound
+     */
+    setBounds ( low, high ) {
+
+        low = parseFloat( low );
+        high = parseFloat( high );
+
+        this.lowerBound = Math.min( low, high );
+        this.upperBound = Math.max( low, high );
+
+        this.is = false;
+
+    };
+
+    /**
+     * set maximum number of ticks
+     * 
+     * @param {Number} ticks number of ticks
+     */
+    setMaxTicks ( ticks ) {
+
+        this.maxTicks = parseInt( ticks );
+
+        this.is = false;
+
+    };
+
+    /**
+     * set logarithmic base
+     * 
+     * @param {Number} base logarithmic base
+     */
+    setBase ( base ) {
+
+        this.base = parseFloat( base );
+
+        this.is = false;
 
     };
 
