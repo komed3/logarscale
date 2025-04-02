d5fd951ba3296566758936e804618de36e0a3f92
komed3 <pk97@outlook.de>
2025-04-03 00:01:07 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * logscale
+ * lightweight npm package to create logarithmic scales
+ * 
+ * @author komed3 (Paul Köhler)
+ * @version 1.0.1
+ * @license MIT
+ */
+
+'use strict';
+
+module.exports = class LogScale {
+
+    is = false;
+
+    constructor ( _low, _high, _ticks, _base ) {
+
+        //
+
+    };
+
+};
